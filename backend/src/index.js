3078fcbcac8e99c3af98755d406455ce929f674b
Benjamin <benferanmiopafunso@gmail.com>
2025-03-21 21:00:18 +0100

    modifying the preflight

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,23 +22,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// // Manually Handle Preflight Requests this is for when i am using vercel
-// app.use((req, res, next) => {
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//         res.header("Access-Control-Allow-Origin", origin);
-//     }
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Authorization"
-//     );
-//     res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies
-//     if (req.method === "OPTIONS") {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// });
+// Manually Handle Preflight Requests this is for when i am using vercel
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+    }
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 
 app.use(express.json())
